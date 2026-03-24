ef727c6f5d0cd8f9ae4a15b165346e5dd415614c
Your Name <you@example.com>
2026-03-24 19:29:38 +0100

    completed reset testing

--- myreactproject/src/Header.jsx
+++ myreactproject/src/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
       <section id="home">
         <header className="header">
           <div className="header-content">
-            <h1>Welcome to My Portfolioss</h1>
+            <h1>Welcome to My Portfolio</h1>
             <h2>Transforming Ideas into Cloud Solutions</h2>
             <p className="header-duties">Explore my journey in cloud engineering and infrastructure automation</p>
             <div className="header-button">
