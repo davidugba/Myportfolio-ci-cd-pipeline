d2675970afb4c588cd6eb5ec596f61b2fbeb425e
Your Name <you@example.com>
2026-03-29 12:54:18 +0200

    updating some section

--- myreactproject/src/Hero.jsx
+++ myreactproject/src/Hero.jsx
@@ -8,7 +8,7 @@ function Hero(){
       <div className="container hero-container">
         <div className="hero-content">
           <h1>Hi, I'm David Ugba</h1>
-          <h2>Cloud Engineer | DevOps Practitioner</h2>
+          <h2>Cloud Engineer | DevOps Practitionerss</h2>
           <p>I build beautiful, responsive web applications with React</p>
           <div className="hero-buttons">
             <a href="#contact" className="btn btn-primary">Get in touch</a>
